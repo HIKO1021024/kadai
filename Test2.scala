a5d931a41522dbe8125181d5a60d39148719d711
EC2 Default User <ec2-user@ip-172-31-5-216.ap-southeast-1.compute.internal>
2018-03-18 08:27:23 +0000

    より行数を減らしてみる

--- Test2.scala
+++ Test2.scala
@@ -1,7 +1,7 @@
 //おそらくこの問題はいわゆる「組み合わせ」を問うものだと想定される。
-//よって母数を限りなく減らした上で、組み合わせのリストを生成し、
+//よって母数をできる限り減らした上で、組み合わせのリストを生成し、
 //そのリストからA[i]<A[j]を精査する方法が効率が良い。
-//精査する方法だが
+
 //与えられる配列は必ず2要素以上入っているものを想定している
 
 import scala.util.Random
@@ -11,39 +11,30 @@ import scala.util.control.Breaks
     def main(args: Array[String]){
       //まずはランダムな配列Aを作成
       val r = new Random
+      //var A: Array[Int] = Array(1,2,3,4,5) //存在しない場合のテスト用
       var A: Array[Int] = Array.empty
       for(i <- 0 to 3000) A = A :+ r.nextInt(100)
       //Kも生成
       val K = r.nextInt(100)
-      //重複は削除する
-      A = A.distinct
-      //配列の最低値を取得
-      val mn = A.min;
       //最低値からKへの必要な数値を算出
-      val T = K - mn 
-      //0+(T+最低値)よりも大きな数値は明らかに条件に合致しないため、削除する
-      if(mn < 0) {
-        //最低値が負の数字の場合
-        A = A.filter(_ <= 0+(T + mn * -1) );
-      }else {
-        //そうでない場合
-        A = A.filter(_ <= 0 + T - mn );
-      }
-      //昇順に並び替える
-      A = A.sorted
-      //Aの中から組み合わせを作成
-      var B = A.combinations(2).toArray
+      val T = K - A.min
+      //A.min・←ここまでには最低限必要値Tが必要→・K
+      //つまり、A[i]orA[j]が最低限必要値Tよりも大きな数値は現時点で不要と判断できるため、削除する
+      //さらに昇順に並び替える
+      //そしてAの中から組み合わせを作成
+      var B = A.filter(_ <= T).sorted.combinations(2).toArray
       //組み合わせの中から条件に合致する数値が存在するか調べる
       //昇順に並び変えているので、組み合わせの数は必ずA[i]<A[j]となり、かつ重複の組み合わせは無い。
-      //iとjで繰り返し処理で探すより効率が良い。
       var flag = false
       val b = new Breaks
       b.breakable {
         B.foreach( { i =>
-        if(i.sum == K ){
-          flag = true
-          //最初にヒットした時点で検索を終了
-          b.break
+          if(i.sum == K ){
+            flag = true
+            println(i.sum)
+            println(K)
+            //最初にヒットした時点で検索を終了(処理速度向上のため)
+            b.break
           }
         })
       }
